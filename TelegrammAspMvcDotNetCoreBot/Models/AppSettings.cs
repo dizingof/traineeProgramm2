8340f678e15177c74107897f6696feaafc1e3828
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-04-29 12:41:38 +0300

    tele bot worked

--- TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
+++ TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
@@ -7,7 +7,7 @@ namespace TelegrammAspMvcDotNetCoreBot.Models
 {
     public class AppSettings
     {
-        public static string Url { get; set; } = "https://mytestlab1.site:443/";
+        public static string Url { get; set; } = "https://mytestlab1.site:443/{0}";
         public static string Name { get; set; } = "@RN_test_bot>";
         public static string Key { get; set; } = "792549344:AAFqAWUnIH9Uitfu_-T-ZyiNhuP_-ElBgmI";
     }
